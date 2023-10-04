8a7c152530c15732680492d32210626c80bacab6
daqh <valeriodipasq@gmail.com>
2023-10-04 13:45:07 +0200

    Update multiprocessing

--- __main__.py
+++ __main__.py
@@ -4,8 +4,9 @@ from torch import nn
 from dgmh import DGMH
 from dgmh.models import Decoder, HGCNEncoder, HyperedgeAutoEncoder, HyperedgeSizeDecisionModule
 from dgmh.utils import load_dataset, compute_embeddings, compute_edge_index
+from time import time
 
-def main(dataset_name, device):
+def main(dataset_name, device, train, generate):
     hyperedges = load_dataset(dataset_name)
 
     num_nodes = max([max(x) for x in hyperedges])
@@ -29,16 +30,34 @@ def main(dataset_name, device):
 
     print()
     autoencoder = HyperedgeAutoEncoder(HGCNEncoder(num_nodes, 64, 32, hyperedges), Decoder(32, 2048, num_nodes))
+    if not train:
+        autoencoder.load_state_dict(torch.load(f'models/{dataset_name}.autoencoder.pth'));
     print(autoencoder)
 
-    hsdm = HyperedgeSizeDecisionModule(num_nodes, 32, max_hyperedge_size)
+    hsdm = HyperedgeSizeDecisionModule(num_nodes, 512, 512, max_hyperedge_size)
+    if not train:
+        pass
+        # autoencoder.load_state_dict(torch.load(f'models/{dataset_name}.hyperedge_size_decision_module.pth'));
     print()
     print(hsdm)
 
+    F = nn.Linear(32, 32).to(device)
+
+    Z = torch.randn(25000, 32)
+
+    begin = time()
+    DGMH(autoencoder.decoder, F, hsdm, Z)
+    print(f'Time: {time() - begin}')
+
 if __name__ == "__main__":
+    torch.multiprocessing.set_sharing_strategy('file_system')
+    torch.multiprocessing.set_start_method('spawn')
+
     parser = argparse.ArgumentParser(description="Train a Hyperedge Autoencoder.")
     parser.add_argument("--dataset", type=str, default="email-Eu", help="Dataset to use.")
     parser.add_argument("--device", default="cpu", type=str, help="Device to use.")
     parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train for.")
+    parser.add_argument("--train", action="store_true", help="Train the model.")
+    parser.add_argument("--generate", action="store_true", help="Generate a new hypergraph.")
     args = parser.parse_args()
-    main(args.dataset, args.device)
+    main(args.dataset, args.device, args.train, args.generate)
--- dgmh/__init__.py
+++ dgmh/__init__.py
@@ -1,8 +1,34 @@
 import torch
 from torch import nn
-from multiprocessing.pool import ThreadPool
+from multiprocessing.pool import Pool
+from time import time
 
-def DGMH(decoder: nn.Module, F: nn.Module, A:nn.Module, Z: torch.Tensor, n: int):
+def generate_hyperedge(decoder: nn.Module, F: nn.Module, A: nn.Module):
+    def wrapper(z):
+        X_i = decoder(z) # X_i = f(F(Z_i))
+        k = torch.argmax(A(X_i.view(1, -1)), dim=1).item() + 1
+        print(f'Size k of the hyperedge: {k} - {time()}', end='\r')
+        theta_i = torch.softmax(X_i, dim=0)
+        hyperedge = []
+        for j in range(k):
+            e_ij = torch.distributions.Categorical(theta_i).sample()
+            hyperedge.append(e_ij.item())
+        return hyperedge
+    return wrapper
+
+def generate_hyperedge(args):
+    decoder, F, A, z = args
+    X_i = decoder(z) # X_i = f(F(Z_i))
+    k = torch.argmax(A(X_i.view(1, -1)), dim=1).item() + 1
+    print(f'Size k of the hyperedge: {k} - {time()}', end='\r')
+    theta_i = torch.softmax(X_i, dim=0)
+    hyperedge = []
+    for j in range(k):
+        e_ij = torch.distributions.Categorical(theta_i).sample()
+        hyperedge.append(e_ij.item())
+    return hyperedge
+
+def DGMH(decoder: nn.Module, F: nn.Module, A:nn.Module, Z: torch.Tensor):
     '''
     decoder: nn.Module
         Trained decoder module
@@ -16,17 +42,10 @@ def DGMH(decoder: nn.Module, F: nn.Module, A:nn.Module, Z: torch.Tensor, n: int)
         Number of hyperedges
     '''
     hyperedges = []
-    for i in range(n):
-        print(f'Hyperedge {i+1}')
-        X_i = decoder(Z[i]) # X_i = f(F(Z_i))
-        k = torch.argmax(A(X_i.view(1, -1)), dim=1).item() + 1
-        print(f'Size k of the hyperedge: {k}')
-        theta_i = torch.softmax(X_i, dim=0)
-        hyperedge = []
-        for j in range(k):
-            e_ij = torch.distributions.Categorical(theta_i).sample()
-            hyperedge.append(e_ij.item())
-        hyperedges.append(hyperedge)
+    args = [(decoder, F, A, z) for z in Z]
+    with Pool(3) as pool:
+        for hyperedge in pool.map(generate_hyperedge, args):
+            hyperedges.append(hyperedge)
     # Remove duplicate nodes
     hyperedges = [sorted(list(set(hyperedge))) for hyperedge in hyperedges]
     return hyperedges
