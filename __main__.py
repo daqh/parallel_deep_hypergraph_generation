3ac5edf5093ea66a0d6f35cd79cdf54728bce12c
daqh <valeriodipasq@gmail.com>
2023-10-05 18:48:44 +0200

    Add DGMH speed evaluation

--- __main__.py
+++ __main__.py
@@ -6,7 +6,21 @@ from dgmh.models import Decoder, HGCNEncoder, HyperedgeAutoEncoder, HyperedgeSiz
 from dgmh.utils import load_dataset, compute_embeddings, compute_edge_index
 from time import time
 
-def main(dataset_name, device, train, generate):
+def evaluate_speed(autoencoder: HyperedgeAutoEncoder, hsdm, F, processes, num_experiments=5):
+    result = []
+    for j in range(num_experiments):
+        print(f"Running experiment {j + 1} for process count {processes}")
+        Z = torch.randn(25000, 32)
+        begin = time()
+        DGMH(autoencoder.decoder, F, hsdm, Z, processes)
+        end = time() - begin
+        result.append(end)
+    print(result)
+    print(f"Average time: {sum(result) / len(result)}")
+    print(f"Max time: {max(result)}")
+    print(f"Min time: {min(result)}")
+
+def main(dataset_name, device, action, processes):
     hyperedges = load_dataset(dataset_name)
 
     num_nodes = max([max(x) for x in hyperedges])
@@ -28,36 +42,44 @@ def main(dataset_name, device, train, generate):
 
     print("Max hyperedge size:", max_hyperedge_size)
 
-    print()
     autoencoder = HyperedgeAutoEncoder(HGCNEncoder(num_nodes, 64, 32, hyperedges), Decoder(32, 2048, num_nodes))
-    if not train:
+    if action == "train":
         autoencoder.load_state_dict(torch.load(f'models/{dataset_name}.autoencoder.pth'));
-    print(autoencoder)
+    autoencoder.eval()
 
     hsdm = HyperedgeSizeDecisionModule(num_nodes, 512, 512, max_hyperedge_size)
-    if not train:
+    if action == "train":
         pass
         # autoencoder.load_state_dict(torch.load(f'models/{dataset_name}.hyperedge_size_decision_module.pth'));
-    print()
-    print(hsdm)
+    hsdm.eval()
+
 
     F = nn.Linear(32, 32).to(device)
 
-    Z = torch.randn(25000, 32)
+    if action == "generate":
+        print()
+        print(f"Number of processes: {processes}")
 
-    begin = time()
-    DGMH(autoencoder.decoder, F, hsdm, Z)
-    print(f'Time: {time() - begin}')
+        Z = torch.randn(25000, 32)
+
+        begin = time()
+        DGMH(autoencoder.decoder, F, hsdm, Z, processes)
+        print(f'Time: {time() - begin}')
+
+    if action == "evaluate_speed":
+        hsdm.share_memory()
+        autoencoder.share_memory()
+        evaluate_speed(autoencoder, hsdm, F, processes)
 
 if __name__ == "__main__":
-    torch.multiprocessing.set_sharing_strategy('file_system')
-    torch.multiprocessing.set_start_method('spawn')
+    torch.multiprocessing.set_sharing_strategy('file_descriptor')
+    torch.multiprocessing.set_start_method('spawn', True)
 
     parser = argparse.ArgumentParser(description="Train a Hyperedge Autoencoder.")
     parser.add_argument("--dataset", type=str, default="email-Eu", help="Dataset to use.")
     parser.add_argument("--device", default="cpu", type=str, help="Device to use.")
     parser.add_argument("--epochs", type=int, default=100, help="Number of epochs to train for.")
-    parser.add_argument("--train", action="store_true", help="Train the model.")
-    parser.add_argument("--generate", action="store_true", help="Generate a new hypergraph.")
+    parser.add_argument("--action", type=str, default="train", help="Action to perform.")
+    parser.add_argument("--processes", type=int, default=1, help="Number of processes to use.")
     args = parser.parse_args()
-    main(args.dataset, args.device, args.train, args.generate)
+    main(args.dataset, args.device, args.action, args.processes)
--- dgmh/__init__.py
+++ dgmh/__init__.py
@@ -2,33 +2,20 @@ import torch
 from torch import nn
 from multiprocessing.pool import Pool
 from time import time
+import os, psutil
 
-def generate_hyperedge(decoder: nn.Module, F: nn.Module, A: nn.Module):
-    def wrapper(z):
-        X_i = decoder(z) # X_i = f(F(Z_i))
-        k = torch.argmax(A(X_i.view(1, -1)), dim=1).item() + 1
-        print(f'Size k of the hyperedge: {k} - {time()}', end='\r')
-        theta_i = torch.softmax(X_i, dim=0)
-        hyperedge = []
-        for j in range(k):
-            e_ij = torch.distributions.Categorical(theta_i).sample()
-            hyperedge.append(e_ij.item())
-        return hyperedge
-    return wrapper
 
 def generate_hyperedge(args):
     decoder, F, A, z = args
     X_i = decoder(z) # X_i = f(F(Z_i))
     k = torch.argmax(A(X_i.view(1, -1)), dim=1).item() + 1
-    print(f'Size k of the hyperedge: {k} - {time()}', end='\r')
+    # print(f'Size k of the hyperedge: {k} - {time()}', end='\r')
     theta_i = torch.softmax(X_i, dim=0)
-    hyperedge = []
-    for j in range(k):
-        e_ij = torch.distributions.Categorical(theta_i).sample()
-        hyperedge.append(e_ij.item())
+    cat = torch.distributions.Categorical(theta_i)
+    hyperedge = list(cat.sample((k,)).detach().cpu().numpy())
     return hyperedge
 
-def DGMH(decoder: nn.Module, F: nn.Module, A:nn.Module, Z: torch.Tensor):
+def DGMH(decoder: nn.Module, F: nn.Module, A:nn.Module, Z: torch.Tensor, processes: int = 1):
     '''
     decoder: nn.Module
         Trained decoder module
@@ -42,10 +29,19 @@ def DGMH(decoder: nn.Module, F: nn.Module, A:nn.Module, Z: torch.Tensor):
         Number of hyperedges
     '''
     hyperedges = []
-    args = [(decoder, F, A, z) for z in Z]
-    with Pool(3) as pool:
-        for hyperedge in pool.map(generate_hyperedge, args):
+    max_memory_usage = psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2
+    if processes == 1:
+        for z in Z:
+            max_memory_usage = max(max_memory_usage, psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+            hyperedge = generate_hyperedge((decoder, F, A, z))
             hyperedges.append(hyperedge)
+    else:
+        args = [(decoder, F, A, z) for z in Z]
+        with Pool(processes) as pool:
+            for hyperedge in pool.map(generate_hyperedge, args):
+                max_memory_usage = max(max_memory_usage, psutil.Process(os.getpid()).memory_info().rss / 1024 ** 2)
+                hyperedges.append(hyperedge)
+    print(f'Max memory usage: {max_memory_usage} MB')
     # Remove duplicate nodes
     hyperedges = [sorted(list(set(hyperedge))) for hyperedge in hyperedges]
     return hyperedges
